4fdfb37e9e9554813dd1df7565e7eaada6b2a2be
Arnav Prasad <122086367+Arnav2Prasad@users.noreply.github.com>
2024-06-15 03:31:51 +0530

    Create grad_ques1.py

---
+++ PDSA/programming/week1/grad_ques1.py
@@ -0,0 +1,42 @@
+QUESTION :
+Write a function find_Min_Difference(L, P) that accepts a list L of integers and P (positive integer) where the size of L is greater than P. The task is to pick P different elements from the list L, where the difference between the maximum value and the minimum value in selected elements is minimum compared to other differences in possible subset of p elements. The function returns this minimum difference value.
+Note - The list can contain more than one subset of p elements that have the same minimum difference value.
+
+Example
+
+Let L = [3, 4, 1, 9, 56, 7, 9, 12, 13] and P = 5
+
+If we see the following two subsets of 5 elements from L
+
+[3, 4, 7, 9, 9] or [7, 9, 9, 12, 13]
+
+Here, the difference between the maximum value and the minimum value in both subset is 9 - 3 = 6 or 13 - 7 = 6 which is minimum. So the output will be 6.
+
+Sample Input
+
+[3, 4, 1, 9, 56, 7, 9, 12]
+5
+
+Output
+
+6
+
+-----------------------------------------------------------------------
+SOLUTION:
+
+def find_Min_Difference(L,p):
+    L.sort()
+    #print(L)
+    min=float('inf')
+    for i in range(len(L)-p+1):
+        if min > L[i+p-1] - L[i]:
+            min = L[i+p-1] - L[i]
+    return min
+        
+    
+    
+L=eval(input().strip())
+P=int(input())
+print(find_Min_Difference(L,P))
+
+
